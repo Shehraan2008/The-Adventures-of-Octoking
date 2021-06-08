8b996fd48a716cd7e33dc467178e140e5e86e235
Shehraan2008 <shehraanrathee21@gmail.com>
2021-06-08 19:12:04 +0530

    Adding Images and sprites

--- app/js/form.js
+++ app/js/form.js
@@ -1,17 +1,33 @@
 class Form {
-  // Creating new DOM Elements for instruction
-  // title, greeting, buttons and inputs.
   constructor() {
     this.title = createElement("h1", "The Adventures of Octoking");
+    this.title.class("gameTitle");
     this.subtitle = createElement(
       "h2",
       "A Turn Based Game by Quiet Slug from Beige Land."
     );
-    this.greeting = createElement("h2");
+    this.subtitle.class("gameSubTitle");
     this.userName = createInput("Nick Name");
     this.startGame = createButton("Enter World");
+    this.startGame.class("nes-btn is-primary");
   }
 
-  // Positoning the Elements
-  display() {}
+  hide() {
+    this.title.hide();
+    this.subtitle.hide();
+    this.userName.hide();
+    this.startGame.hide();
+  }
+
+  display() {
+    this.title.position(width / 4 - 400, height / 8);
+    this.subtitle.position(width / 8, height / 3 + 100);
+    this.userName.position(width / 3 + 70, height / 1.5);
+    this.startGame.position(width / 3 + 120, height / 1.5 + 80);
+
+    this.startGame.mousePressed(() => {
+      form.hide();
+      gameState = 1;
+    });
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,47 @@
 let gameState = 0;
+let backgroundImg;
 let form;
+let octocat, octopus, octocatAni, octopusAni;
 
 // Loading Image Assets.
-function preload(params) {}
+function preload(params) {
+  // Animaitons
+  octocatAni = loadAnimation(
+    "./app/assets/octocat/cat1.png",
+    "./app/assets/octocat/cat2.png"
+  );
+
+  octopusAni = loadAnimation(
+    "./app/assets/octopus/octopus1.png",
+    "./app/assets/octopus/octopus2.png"
+  );
+
+  // Images.
+  backgroundImg = loadImage("./app/assets/background/startBg.jpeg");
+
+  // Audios
+}
 
 function setup(params) {
-  const canvas = createCanvas(windowWidth - 20, windowHeight - 20);
+  const canvas = createCanvas(1450, 630);
 
+  // The Sprites
+  octocat = createSprite(200, 450, 50, 50);
+  octocat.addAnimation("octopus_moving", octocatAni);
+  octocat.scale = 0.4;
+
+  octopus = createSprite(1200, 450, 50, 50);
+  octopus.addAnimation("octopus_moving", octopusAni);
+  octopus.scale = 2;
+
+  // Main game
   form = new Form();
   form.display();
 }
 
 function draw(params) {
-  background(0);
+  // background("beige");
+  background(backgroundImg);
+
+  drawSprites();
 }
